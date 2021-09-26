08869a5b0149080e081bbc0793c3919710e599a9
doraluz <33816619+doraluz@users.noreply.github.com>
2021-09-26 17:43:31 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,70 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . . . # #
+            . . . . #
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # . .
+            # # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+    } else if (input.isGesture(Gesture.Shake)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
